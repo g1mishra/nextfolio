989ff18df48b9b6f5620564955f11518cb2ec029
Jeevan Kumar <jmishrajeevan8969@gmail.com>
2022-11-17 15:07:53 +0530

    minor change

--- components/about-me/bio.tsx
+++ components/about-me/bio.tsx
@@ -2,7 +2,7 @@ const Bio = () => {
   return (
     <>
       <p className="inline">
-        I am Jeevan Kumar and a full-stack web developer from India with more than 2.5 years of work
+        I am Jeevan Kumar, a full-stack web developer from India with more than 2.5 years of work
         experience. I enjoy working in environments where I can apply my knowledge, energy, and
         technical skills to a new learning experience. JavaScript, React.js, Next.js, Node.js, and
         Python are my strongest skills.
