8f1d188f293548c0310470ea88eb33835d0c95ed
Jeevan Kumar <jmishrajeevan8969@gmail.com>
2023-01-10 00:55:21 +0530

    bio text update

--- components/about-me/bio.tsx
+++ components/about-me/bio.tsx
@@ -7,6 +7,6 @@ const Bio = () => {
   );
 };
 
-export const BIO_TEXT = `I am Jeevan Kumar, a full-stack web developer from India with more than 2.5 years of work experience. I enjoy working in environments where I can apply my knowledge, energy, and technical skills to a new learning experience. JavaScript, React.js, Next.js, Node.js, and Python are my strongest skills. I like to talk about technology, coding, and development on YouTube in my spare time.`;
+export const BIO_TEXT = `Hello! My name is Jeevan Kumar, and I am a full-stack web developer based in India with over 2.5 years of experience. I have a strong skillset in technologies such as JavaScript, React.js, Next.js, Node.js, and Python, and I am always eager to learn and grow as a developer. In my spare time, I enjoy sharing my love for technology, coding, and development through my YouTube channel. I am passionate about working on projects that allow me to utilize my skills and knowledge to create innovative solutions.`;
 
 export default Bio;
